ca14868411215a9cc03801ac9f524a9152133a8d
jasmin.shaikh <jasminmshaikh22@gmail.com>
2025-12-26 21:58:20 +0530

    FlyWay script

--- IssueTracker/src/main/java/com/IssueTracker/IssueTracker/Exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.IssueTracker.IssueTracker.Exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public UserNotFoundException(String msg) {
-		super(msg);
-	}
-}
--- IssueTracker/src/main/java/com/IssueTracker/IssueTracker/repository/TeamRepository.java
+++
@@ -1,9 +0,0 @@
-package com.IssueTracker.IssueTracker.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.IssueTracker.IssueTracker.Entity.Team;
-
-public interface TeamRepository extends JpaRepository<Team,Long>{
-
-}
